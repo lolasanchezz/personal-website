15b0189aa55dcb99a8f38814f90ad7cfff4873d4
lolasanchezz <lolansanchez@icloud.com>
2024-08-20 19:39:52 -0400

    error with mime/converted to ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+//making cursor move
+console.log("run")
+function adjustWidth(){
+    const textArea = document.getElementById('termInput') as HTMLInputElement;
+    const letterLength = textArea.value.length;
+}
+
