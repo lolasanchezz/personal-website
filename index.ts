708b377a02f6e795e770281119f59bf7771a04d2
lolasanchezz <lolansanchez@icloud.com>
2024-08-20 20:08:49 -0400

    adjustWidth not running for unknown reasons + ts completely set up and no more mime errors

--- index.ts
+++ index.ts
@@ -1,7 +1,16 @@
 //making cursor move
 console.log("run")
-function adjustWidth(){
-    const textArea = document.getElementById('termInput') as HTMLInputElement;
+
+
+const textArea = document.getElementById('termInput') as HTMLInputElement;
+textArea.addEventListener("oninput", () => {
+
+   
     const letterLength = textArea.value.length;
-}
+
+    const widthOfInput = parseInt(document.getElementById('input').style.width);
+    console.log("e")
+    textArea.style.width = ((letterLength * (0.5 * widthOfInput)).toString()) + "px";
+});
+
 
